a889ec85e085afc28bc64ea3bf76f9cbce485030
valeriewan23 <104895441+valeriewan23@users.noreply.github.com>
2022-06-23 14:41:52 +0800

    Update test_circle_area.R

--- tests/testthat/test_circle_area.R
+++ tests/testthat/test_circle_area.R
@@ -1,11 +1,3 @@
-options(repos = list(CRAN="http://cran.rstudio.com/"))
-
-install.packages("devtools")
-library(devtools)
-
-devtools::install_github("valeriewan23/testing")
-library(testing)
-
-r <- 5
-
-circle_area(r)
+test_that("circle area", {
+    expect_equal(circle_area(5), 78.5)
+})
