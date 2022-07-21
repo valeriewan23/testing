4b4518692dc05d780b07ca480f1def4504e7bbd8
valeriewan23 <104895441+valeriewan23@users.noreply.github.com>
2022-07-21 16:00:33 +0800

    Update test_circle.R

--- tests/testthat/test_circle.R
+++ tests/testthat/test_circle.R
@@ -1,3 +1,9 @@
 test_that("circle area", {
     expect_equal(circle_area(5), 78.5)
 })
+
+test_that("circle area range", {
+    range <- c(1,2,3)
+    
+    expect_equal(circle_area(range), c(3.14, 12.56,28.26))
+})
