e6891d39d0ef044036dad3c60b101d5c6dce627f
valeriewan23 <104895441+valeriewan23@users.noreply.github.com>
2022-07-22 18:03:55 +0800

    Update test_circle.R

--- tests/testthat/test_circle.R
+++ tests/testthat/test_circle.R
@@ -7,3 +7,10 @@ test_that("circle area range", {
     
     expect_equal(circle_area(range), c(3.14, 12.56,28.26))
 })
+
+test_that("circle area df", {
+    df <- data.frame(number  = c(1, 2, 3),
+                  value = c(4, 5, 6))
+    
+    expect_equal(circle_area(df$value), c(50.24, 78.50, 113.04))
+})
